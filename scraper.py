cc231e535f563ab04765ebbbdc6964e1c0a9f162
Luis Perez <luis.perez.live@gmail.com>
2018-11-25 14:05:05 -0800

    Implemented trivial scraper

---
+++ scraper.py
@@ -0,0 +1,92 @@
+import mintapi
+import os
+import pandas as pd
+import pygsheets
+
+from typing import NamedTuple
+
+_JOINT_SPENDING_ACCOUNTS = [
+    'Spark Visa Signature Business', 'Amazon Card - Luis', 'TOTAL_CHECKING',
+    'Citi - Personal', 'Preferred', 'Freedom - Belinda', 'Mariott Rewards',
+    'Freedom Unlimited - Belinda', 'Freedom', 'Amazon Store Card'
+]
+_COLUMNS = ['odate', 'mmerchant', 'amount', 'category']
+_COLUMN_NAMES = ['Date', 'Description', 'Amount', 'Category']
+
+_RAW_SHEET_TITLE = "Raw - All Transactions"
+_KEYS_FILE = 'keys.json'
+
+
+class Credentials(NamedTuple):
+  email: str
+  password: str
+
+
+def _GetCredentials() -> Credentials:
+  """Retrieves the crendentials for logging into Mint. 
+
+  This is necessary because they do not currently provide an API.
+
+  Returns:
+    The retrieved crendentials
+  """
+  return Credentials(
+      email=os.environ.get('MINT_EMAIL'),
+      password=os.environ.get('MINT_PASSWORD'))
+
+
+def _RetrieveTransactions(creds: Credentials) -> pd.DataFrame:
+  """Retrieves all Mint transactions using the given credentials.
+
+  The functions also cleans and prepares the transactions to match
+  the format expected by Google sheets.
+
+  Args:
+    creds: The Credentials object to use for loging into Mint
+
+  Returns:
+    A data frame of all mint transactions"""
+  mint = mintapi.Mint(
+      creds.email,
+      creds.password,
+      mfa_method='sms',
+      headless=True,
+      mfa_input_callback=None)
+  transactions = mint.get_detailed_transactions(
+      skip_duplicates=True, remove_pending=True)
+
+  spend_transactions = transactions[transactions.account.isin(
+      _JOINT_SPENDING_ACCOUNTS) & transactions.isSpending]
+  spend_transactions = spend_transactions[_COLUMNS]
+  spend_transactions.columns = _COLUMN_NAMES
+
+  # Credit card payments are redundant.
+  spend_transactions = spend_transactions[
+      spend_transactions.Category != 'Credit Card Payment']
+  # Flip expenditures so they're negative.
+  spend_transactions.Amount = -1 * spend_transactions.Amount
+  return spend_transactions.sort_values('Date', ascending=True)
+
+
+def _UpdateGoogleSheet(sheet: pygsheets.Sheet, data: pd.DataFrame) -> None:
+  """Updates the given transactions sheet with the transactions data
+
+  Args:
+    sheet: The sheet containing our transaction analysis and visualization.
+    data: The new, cleaned, raw transaction data to analyze.
+  """
+  all_data_ws = sheet.worksheet_by_title(title=_RAW_SHEET_TITLE)
+  all_data_ws.set_dataframe(data, 'A1', fit=True)
+  all_data_ws.sync()
+
+
+def main():
+  creds: Credentials = _GetCredentials()
+  latestTransactions: pd.DataFrame = _RetrieveTransactions(creds=creds)
+
+  sheet = pygsheets.authorize(service_file=_KEYS_FILE)
+  UpdateGoogleSheet(sheet=sheet, data=latestTransactions)
+
+
+if __name___ == '__main__':
+  main()

7e55b4a7986c736ff154a17d55990549286bcbf6
Luis Perez <luis.perez.live@gmail.com>
2019-10-23 09:11:13 -0700

    Add more accounts to the white list

--- config.py
+++ config.py
@@ -5,42 +5,51 @@ from typing import List, Optional, Text
 
 class Config:
   """A class capturing configurable settings for the mint scraper.
-  """
+        """
 
   def __init__(self: 'Config') -> None:
     """Initializes the config to the default values
 
-    Properties:
-      JOINT_SPENDING_ACCOUNTS: The account names which are held jointly.
-        Only transactions from accounts matching these are uploaded to
-        the Google Sheet.
+                Properties:
+                  JOINT_SPENDING_ACCOUNTS: The account names which are held jointly.
+                    Only transactions from accounts matching these are uploaded to
+                    the Google Sheet.
 
-      COLUMNS: The column names of the retrieved dataframe return by the
-        mintapi library.
-      COLUMN_NAMES: COLUMN_NAMES[i] is the column name in the Google sheet
-        of the corresponding COLUMNs[i]
-      RAW_SHEET_TITLE: The name of the sheet which contains the raw
-        transactions.
-      SETTINGS_SHEET_TITLE: The name of the sheet containing the setting page.
-      KEYS_FILE: The file path (relative) of the keys to use when using the
-        Google Sheets API
-      WORKSHEET_TITLE: The title of the worksheet in Google Drive where
-        transactions are uploaded.
-      IGNORED_MERCHANTS: A list of merchants which are filtered out when
-        processing transactions.
-      IGNORED_CATEGORIES: A list of categories which are filtered out when
-        processing transactions.
-      SESSION_PATH: The full path to where Chrome session data can be stored
-        and retrieved from.
-      IMAP_SERVER: The IMAP server to use for MFA using email.
-      WAIT_FOR_ACCOUNT_SYNC: Whether or not to wait for account syncing.
+                  COLUMNS: The column names of the retrieved dataframe return by the
+                    mintapi library.
+                  COLUMN_NAMES: COLUMN_NAMES[i] is the column name in the Google sheet
+                    of the corresponding COLUMNs[i]
+                  RAW_SHEET_TITLE: The name of the sheet which contains the raw
+                    transactions.
+                  SETTINGS_SHEET_TITLE: The name of the sheet containing the setting page.
+                  KEYS_FILE: The file path (relative) of the keys to use when using the
+                    Google Sheets API
+                  WORKSHEET_TITLE: The title of the worksheet in Google Drive where
+                    transactions are uploaded.
+                  IGNORED_MERCHANTS: A list of merchants which are filtered out when
+                    processing transactions.
+                  IGNORED_CATEGORIES: A list of categories which are filtered out when
+                    processing transactions.
+                  SESSION_PATH: The full path to where Chrome session data can be stored
+                    and retrieved from.
+                  IMAP_SERVER: The IMAP server to use for MFA using email.
+                  WAIT_FOR_ACCOUNT_SYNC: Whether or not to wait for account syncing.
 
-    """
+                """
     self.JOINT_SPENDING_ACCOUNTS: List[Text] = [
-        'Spark Visa Signature Business', 'Amazon Card - Luis',
-        'TOTAL_CHECKING', 'Citi - Personal', 'Preferred', 'Freedom - Belinda',
-        'Mariott Rewards', 'Freedom Unlimited - Belinda', 'Freedom',
-        'Amazon Store Card'
+        'Spark Visa Signature Business',
+        'Amazon Card - Luis',
+        'TOTAL_CHECKING',
+        'Citi - Personal',
+        'Preferred',
+        'Freedom - Belinda',
+        'Mariott Rewards',
+        'Freedom Unlimited - Belinda',
+        'Freedom',
+        'Amazon Store Card',
+        'Bank of America Travel Rewards Signature',
+        'CapitalOne Venture Card',
+        'Marriott Rewards',
     ]
     self.COLUMNS: List[Text] = ['odate', 'mmerchant', 'amount', 'category']
     self.COLUMN_NAMES: List[Text] = ['Date', 'Merchant', 'Amount', 'Category']
@@ -55,8 +64,12 @@ class Config:
     # Paid for Luis' Family's phones are not counted.
     # Ignore SCPD Payments.
     self.IGNORED_MERCHANTS: List[Text] = [
-        'Project Fi', 'Stanford Scpd Ca', 'Stanford Scpd 6507253016 Ca',
-        'Anita Borg Institute', 'Graves Bail Bonds'
+        'Project Fi',
+        'Stanford Scpd Ca',
+        'Stanford Scpd 6507253016 Ca',
+        'Anita Borg Institute',
+        'Graves Bail Bonds',
+        'Stanford Scpd',
     ]
     # Credit card payments are redundant.
     self.IGNORED_CATEGORIES: List[Text] = ['Credit Card Payment']

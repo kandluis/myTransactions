7ee281aaf6cc046a4beba06fa70c8e90039158cd
Luis Perez <luis.perez.live@gmail.com>
2022-09-02 12:56:27 -0700

    fix indentation, refactored code to better support options

--- index.py
+++ index.py
@@ -1,7 +1,11 @@
 # Lambda handler code.
-from scraper import main
+import scraper
 
 
 def lambda_handler(event, context):
     """Handler for when this script is run on AWS Lambda."""
-    main()
+
+    scraper.scrape_and_upload(
+        options=scraper.ScraperOptions(
+            types='all', showBrowser=False, cookies=None)
+    )

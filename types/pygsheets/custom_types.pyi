87de0cbf021bc025d2a6925641c60314225f8f62
Luis Perez <luis.perez.live@gmail.com>
2025-07-02 18:50:05 -0700

    black

--- types/pygsheets/custom_types.pyi
+++ types/pygsheets/custom_types.pyi
@@ -2,55 +2,55 @@ from _typeshed import Incomplete
 from enum import Enum
 
 class WorkSheetProperty(Enum):
-    TITLE = 'TITLE'
-    ID = 'ID'
-    INDEX = 'INDEX'
+    TITLE = "TITLE"
+    ID = "ID"
+    INDEX = "INDEX"
 
 class ValueRenderOption(Enum):
-    FORMATTED_VALUE = 'FORMATTED_VALUE'
-    UNFORMATTED_VALUE = 'UNFORMATTED_VALUE'
-    FORMULA = 'FORMULA'
+    FORMATTED_VALUE = "FORMATTED_VALUE"
+    UNFORMATTED_VALUE = "UNFORMATTED_VALUE"
+    FORMULA = "FORMULA"
 
 class DateTimeRenderOption(Enum):
-    SERIAL_NUMBER = 'SERIAL_NUMBER'
-    FORMATTED_STRING = 'FORMATTED_STRING'
+    SERIAL_NUMBER = "SERIAL_NUMBER"
+    FORMATTED_STRING = "FORMATTED_STRING"
 
 class FormatType(Enum):
     CUSTOM: Incomplete
-    TEXT = 'TEXT'
-    NUMBER = 'NUMBER'
-    PERCENT = 'PERCENT'
-    CURRENCY = 'CURRENCY'
-    DATE = 'DATE'
-    TIME = 'TIME'
-    DATE_TIME = 'DATE_TIME'
-    SCIENTIFIC = 'SCIENTIFIC'
+    TEXT = "TEXT"
+    NUMBER = "NUMBER"
+    PERCENT = "PERCENT"
+    CURRENCY = "CURRENCY"
+    DATE = "DATE"
+    TIME = "TIME"
+    DATE_TIME = "DATE_TIME"
+    SCIENTIFIC = "SCIENTIFIC"
 
 class ExportType(Enum):
-    XLS = 'XLS'
-    ODT = 'ODT'
-    PDF = 'PDF'
-    CSV = 'CSV'
-    TSV = 'TSV'
-    HTML = 'HTML'
+    XLS = "XLS"
+    ODT = "ODT"
+    PDF = "PDF"
+    CSV = "CSV"
+    TSV = "TSV"
+    HTML = "HTML"
 
 class HorizontalAlignment(Enum):
-    LEFT = 'LEFT'
-    RIGHT = 'RIGHT'
-    CENTER = 'CENTER'
+    LEFT = "LEFT"
+    RIGHT = "RIGHT"
+    CENTER = "CENTER"
     NONE: Incomplete
 
 class VerticalAlignment(Enum):
-    TOP = 'TOP'
-    MIDDLE = 'MIDDLE'
-    BOTTOM = 'BOTTOM'
+    TOP = "TOP"
+    MIDDLE = "MIDDLE"
+    BOTTOM = "BOTTOM"
     NONE: Incomplete
 
 class ChartType(Enum):
-    BAR = 'BAR'
-    LINE = 'LINE'
-    AREA = 'AREA'
-    COLUMN = 'COLUMN'
-    SCATTER = 'SCATTER'
-    COMBO = 'COMBO'
-    STEPPED_AREA = 'STEPPED_AREA'
+    BAR = "BAR"
+    LINE = "LINE"
+    AREA = "AREA"
+    COLUMN = "COLUMN"
+    SCATTER = "SCATTER"
+    COMBO = "COMBO"
+    STEPPED_AREA = "STEPPED_AREA"
